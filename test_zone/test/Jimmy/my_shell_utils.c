de50bc875aeaed3f1283e26220e0b4eba4016fb6
Mkpos <emeraldjimmy500@gmail.com>
2023-05-24 10:58:22 -0700

    edited

--- test_zone/test/Jimmy/my_shell_utils.c
+++ test_zone/test/Jimmy/my_shell_utils.c
@@ -131,7 +131,7 @@ void (*get_func(char *command))(char **)
 {
 	int index;
 	function_map mapping[] = {
-		{"env", env}, {"exit", quit}
+		{"env", env}, {"exit", exit}
 	};
 
 	for (index = 0; index < 2; index++)
